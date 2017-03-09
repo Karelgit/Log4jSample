e6bf3849ef0fc783652a609c6dac65f963f3b2a9
huanghai <huanghai@simpletech.com.cn>
2017-03-09 18:33:06 +0800

    修改fileAppender的文件名称

--- src/main/java/Slf4jSample.java
+++ src/main/java/Slf4jSample.java
@@ -1,5 +1,11 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * <类详细说明:log4j示例>
@@ -8,16 +14,34 @@ import org.slf4j.LoggerFactory;
  * @Version: 2017-03-09
  **/
 public class Slf4jSample {
-    public static void main(String[] args) {
+   private static  Logger logger = LoggerFactory.getLogger(Slf4jSample.class);
 
-        System.setProperty("tidParam","12341234124");
-        Logger logger = LoggerFactory.getLogger(Slf4jSample.class);
-        new HelloWorld().getWorldSound();
+    public static void main(String[] args) {
         final String message = "Hello logging!";
+        String fileName = "./logs/"+ 4123421+"_logging.log";
+        new Slf4jSample().updateFichierLogNames(fileName);
+        new HelloWorld().getWorldSound();
         logger.trace(message);
         logger.debug(message);
         logger.info(message);
         logger.warn(message);
         logger.error(message);
+    }
+
+    public  void updateFichierLogNames(String fileName) {
+        Properties props = new Properties();
+        try {
+            InputStream configStream = getClass().getResourceAsStream( "/log4j.properties");
+            props.load(configStream);
+            configStream.close();
+        } catch (IOException e) {
+            System.out.println("Error: Cannot laod configuration file");
+        }
+
+        props.setProperty("log4j.appender.file.File", fileName);
+
+        LogManager.resetConfiguration();
+        PropertyConfigurator.configure(props);
+
     }
 }
